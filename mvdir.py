3fe2a5327d9e9a51641f43fa0595f0a4a957e1f4
KevinStarry <33227207+KevinStarry@users.noreply.github.com>
2021-07-08 17:36:16 +0800

    Update mvdir.py

--- mvdir.py
+++ mvdir.py
@@ -1,14 +1,12 @@
 import os
 import shutil
-
 # /num/withdir/files
-root_dir = os.getcwd()
-print('Current path:', root_dir)
+first_path = os.getcwd()
+print(f'current path:{first_path}\n')
 # print(os.path.abspath(os.path.dirname(__file__)))
-first_dirs = os.listdir(root_dir)
+first_dirs = os.listdir(first_path)
 for _ in first_dirs:
     if os.path.isdir(_):
-        second_dirs = os.listdir(_)
-        for _ in os.listdir(second_dirs):
-            shutil.move(_,root_dir)
-
+        second_path = os.path.abspath(_)
+        print(f'second path is:{second_path}\n')
+        shutil.move(second_path,first_path)
